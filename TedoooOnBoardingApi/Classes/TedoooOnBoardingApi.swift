8c38662ac2e7ffae46fa35bec88d5e27c62ddc01
morapelker <morapelker@gmail.com>
2022-07-07 13:25:22 +0300

    "v1.0.3"

--- TedoooOnBoardingApi/Classes/TedoooOnBoardingApi.swift
+++ TedoooOnBoardingApi/Classes/TedoooOnBoardingApi.swift
@@ -2,12 +2,14 @@ import Combine
 
 public struct GroupSuggestion: Encodable {
     
+    public let id: String
     public let name: String
     public let participants: Int
     public let description: String
     public let image: String?
     
-    public init(name: String, participants: Int, description: String, image: String?) {
+    public init(id: String, name: String, participants: Int, description: String, image: String?) {
+        self.id = id
         self.name = name
         self.participants = participants
         self.description = description
@@ -16,8 +18,30 @@ public struct GroupSuggestion: Encodable {
     
 }
 
+public struct BusinessSuggestion: Encodable {
+    
+    public let id: String
+    public let name: String
+    public let rating: CGFloat
+    public let totalReviews: Int
+    public let categories: [String]
+    public let description: String
+    
+    public init(id: String, name: String, rating: CGFloat, totalReviews: Int, categories: [String], description: String) {
+        self.id = id
+        self.name = name
+        self.rating = rating
+        self.totalReviews = totalReviews
+        self.categories = categories
+        self.description = description
+    }
+    
+}
+
 public protocol TedoooOnBoardingApi {
     
     func getGroupSuggestions() -> AnyPublisher<[GroupSuggestion], Never>
+    
+    func getBusinessSuggestions() -> AnyPublisher<[BusinessSuggestion], Never>
     
 }
